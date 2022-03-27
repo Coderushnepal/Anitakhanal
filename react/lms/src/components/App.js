e20bf025e7f388e858731521cde6c38f5e944d81
anitakhanal <48638291+anitakhanal@users.noreply.github.com>
2022-03-27 08:14:54 +0545

    initial commit

---
+++ react/lms/src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import Dashboard from "./views/Dashboard";
+
+
+
+const App = () => {
+  return (
+    <>
+    <h1> Hello World !!!</h1>
+    <App />
+    <Dashboard />
+    </>
+  )
+}
+
+export default App;
---
+++ react/lms/src/components/views/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Dashboard() {
+  return (
+  <>
+  <div className='header'>
+      <div className='nav'>
+          <button className='btn btn-sucess'> Click me </button>
+      </div>
+
+  </div>
+  </>
+  )
+}
+
+export default Dashboard;
